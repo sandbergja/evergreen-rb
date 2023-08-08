a9282a513c7f0dc0b14a64090c98cc980b32fbf3
Jane Sandberg <js7389@princeton.edu>
2023-08-08 08:17:42 -0700

    Remove mixin directory

--- sig/evergreen/mixins/retrieval_methods.rb
+++
@@ -1,9 +0,0 @@
-class Evergreen
-  module Mixins
-    # Convenience methods to search for data
-    # and initialize objects based on the results
-    module RetrievalMethods
-      def get_bib_record: (untyped id) -> untyped
-    end
-  end
-end
